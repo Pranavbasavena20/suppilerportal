0f9b6450d3579d56a79d29564d2054a6509a2667
Pranav Basavena <pranavbasavena20@gmail.com>
2022-10-21 17:46:06 +0800

    Added Errorhandler.js

--- webapp/Component.js
+++ webapp/Component.js
@@ -5,9 +5,10 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/Device",
-    "com/spm/suppilerportal/model/models"
+    "com/spm/suppilerportal/model/models",
+    "./controller/ErrorHandler",
 ],
-    function (UIComponent, Device, models) {
+    function (UIComponent, Device, models, ErrorHandler) {
         "use strict";
 
         return UIComponent.extend("com.spm.suppilerportal.Component", {
@@ -25,8 +26,28 @@ sap.ui.define([
                 UIComponent.prototype.init.apply(this, arguments);
                 // enable routing
                 this.getRouter().initialize();
+                this._oErrorHandler = new ErrorHandler(this);
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
+            },
+            destroy: function () {
+                this._oErrorHandler.destroy();
+                // call the base component's destroy function
+                UIComponent.prototype.destroy.apply(this, arguments);
+            },
+            getContentDensityClass: function () {
+                if (this._sContentDensityClass === undefined) {
+                    // check whether FLP has already set the content density class; do nothing in this case
+                    if (jQuery(document.body).hasClass("sapUiSizeCozy") || jQuery(document.body).hasClass("sapUiSizeCompact")) {
+                        this._sContentDensityClass = "";
+                    } else if (!Device.support.touch) { // apply "compact" mode if touch is not supported
+                        this._sContentDensityClass = "sapUiSizeCompact";
+                    } else {
+                        // "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+                        this._sContentDensityClass = "sapUiSizeCozy";
+                    }
+                }
+                return this._sContentDensityClass;
             },
 
 
