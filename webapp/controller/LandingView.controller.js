927566adb75017bea7ef4ea6882eceddc44e9503
Pranavbasavena20 <pranavbasavena20@gmail.com>
2022-10-21 16:34:20 +0800

    loginscreen

--- webapp/controller/LandingView.controller.js
+++ webapp/controller/LandingView.controller.js
@@ -1,15 +1,32 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "com/spm/suppilerportal/utils/dataUtil"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel, dataUtil) {
         "use strict";
 
         return Controller.extend("com.spm.suppilerportal.controller.LandingView", {
             onInit: function () {
+                var oModel = dataUtil.createJsonModel()
 
+                oModel.setData({
+                    "bPasswordVi": false
+                });
+                this.getView().setModel(oModel, "oLanding");
+
+            },
+            onfnpressnext: function () {
+                var oModel = this.getView().getModel("oLanding"),
+                    sName = oModel.getProperty("/sUserName");
+                if (sName !== undefined && sName !== null) {
+                    oModel.setProperty("/sPassword", true);
+                } else {
+                    sap.m.MessageBox.error("Please enter a valid user name");
+                }
             }
         });
     });
