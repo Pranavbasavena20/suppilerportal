13a009cec2723571e461b9cafc67acd862ad2c27
Pranav Basavena <pranavbasavena20@gmail.com>
2022-10-31 17:50:42 +0800

    Pranav Commit

--- webapp/controller/LandingView.controller.js
+++ webapp/controller/LandingView.controller.js
@@ -13,27 +13,29 @@ sap.ui.define([
 
         return Controller.extend("com.spm.suppilerportal.controller.LandingView", {
             onInit: function () {
-                var oModel = dataUtil.createJsonModel()
+                var oModel = dataUtil.createJsonModel();
 
                 oModel.setData({
-                    "bPasswordVi": false,
+                    "sNext": true,
+                    "sSubmit": false,
                     "sLogoImage": sap.ui.require.toUrl("com/spm/suppilerportal/css/image/logo.jpeg")
                 });
+
                 this.getView().setModel(oModel, "oLanding");
 
             },
-            onfnpressnext: function () {
-                var oModel = this.getView().getModel("oLanding"),
-                    sName = oModel.getProperty("/sUserName");
-                if (sName !== undefined && sName !== null) {
-                    oModel.setProperty("/bPasswordVi", true);
-                    this.getRouter().navTo("GenericTilesView");
-                } else {
-                    sap.m.MessageBox.error("Please enter a valid user name");
-                }
+            onClickNext: function () {
+                this.getView().getModel("oLanding").setProperty("/sNext", false);
+                this.getView().getModel("oLanding").setProperty("/sSubmit", true);
+                this.getView().byId("idIconTabBar").setSelectedKey("Contact");
+            },
+            onClickSubmit: function () {
+                this.getView().getModel("oLanding").setProperty("/sNext", true);
+                this.getView().getModel("oLanding").setProperty("/sSubmit", false);
+                this.onCancelForm();
             },
             getRouter: function () {
-                return UIComponent.getRouterFor(this);
+                return sap.ui.core.UIComponent.getRouterFor(this);
             },
             onSignUp: function (oEvent) {
                 var that = this;
@@ -45,6 +47,7 @@ sap.ui.define([
                     }).then(function (oDialog) {
                         that._oSignUp = oDialog;
                         that.getView().addDependent(that._oSignUp);
+                        that.getView().getModel("oLanding").setProperty("/sSubmit", false);
                     });
                 }
                 that._SignUpDialog.then(function (oDialog) {
@@ -54,12 +57,18 @@ sap.ui.define([
             },
             onfnpresssubmit: function () {
                 var oModel = this.getView().getModel("oLanding"),
+                    sName = oModel.getProperty("/sUserName"),
                     sPassword = oModel.getProperty("/sPassword");
-                if (sPassword !== undefined && sPassword !== null) {
+                if (sName !== undefined && sName !== null && sPassword !== undefined && sPassword !== null) {
                     this.getRouter().navTo("GenericTilesView");
+                    // this.getOwnerComponent().getRouter().navTo("GenericTilesView");
+
                 } else {
-                    sap.m.MessageBox.error("Please enter a valid password");
+                    sap.m.MessageBox.error("Please enter a valid user name/password");
                 }
+            },
+            onCancelForm: function () {
+                this._oSignUp.close();
             }
         });
     });
