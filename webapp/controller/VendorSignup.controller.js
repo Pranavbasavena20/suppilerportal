b979b1ca1722b4660baf7d8dcd1501df0890f284
Pranav Basavena <pranavbasavena20@gmail.com>
2022-11-02 20:11:25 +0800

    Pranav -Nov 2 2022

--- webapp/controller/VendorSignup.controller.js
+++ webapp/controller/VendorSignup.controller.js
@@ -1,45 +1,79 @@
-<mvc:View controllerName="com.spm.suppilerportal.controller.LandingView" xmlns:mvc="sap.ui.core.mvc" displayBlock="true" xmlns="sap.m" xmlns:layout="sap.ui.layout" xmlns:form="sap.ui.layout.form">
-    <Page id="page" class="backgroundclass" showHeader="false" title="{i18n>title}">
-        <content>
-            <FlexBox justifyContent="Center">
-                <HBox width="30%" justifyContent="SpaceBetween" class="paddingClass">
-                    <RadioButton class="whiteText1" text="Retailer Login" />
-                    <RadioButton class="whiteText1" text="Vendor Login" />
-                </HBox>
-            </FlexBox>
-            <FlexBox justifyContent="Center">
-                <VBox width="30%" height="50%" class="paddingClass marginClass backgroundshadow" justifyContent="Center" backgroundDesign="Solid">
-                    <HBox justifyContent="Center">
-                        <Label design="Bold" text=" Welcome" class="wlcmTxt sapUiSmallMargin" />
-                    </HBox>
-                    <form:SimpleForm layout="ResponsiveGridLayout" labelSpanXL="12" labelSpanL="12" labelSpanM="12" columnsL="1" columnsXL="1" editable="true" adjustLabelSpan="false" singleContainerFullSize="false">
-                        <form:content>
-                            <VBox>
-                                <Label text="User Name" />
-                                <Input value="{oLanding>/sUserName}" />
-                                <layoutData>
-                                    <layout:GridData span="XL6 L6 M6 S12" />
-                                </layoutData>
-                            </VBox>
-                            <VBox>
-                                <Label text="Password" />
-                                <Input type="Password" value="{oLanding>/sPassword}" />
-                                <layoutData>
-                                    <layout:GridData span="XL12 L12 M12 S12" linebreak="false" />
-                                </layoutData>
-                            </VBox>
-                        </form:content>
-                    </form:SimpleForm>
-                    <Button class="btnclass" type="Emphasized" text="Submit" press="onfnpresssubmit" width="100%" />
-                    <HBox justifyContent="Center">
-                        <Text class="sapUiTinyMarginTop" text="New User ? Please" />
-                        <Link press="onSignUp" class="sapUiTinyMarginBegin sapUiTinyMarginTop" text="Signup" />
-                    </HBox>
-                    <HBox justifyContent="Center">
-                        <Link class="sapUiTinyMarginBegin sapUiTinyMarginTop" text="Forgot Password" />
-                    </HBox>
-                </VBox>
-            </FlexBox>
-        </content>
-    </Page>
-</mvc:View>
+sap.ui.define([
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+    "com/spm/suppilerportal/utils/dataUtil",
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/Fragment",
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, JSONModel, dataUtil, UIComponent, Fragment) {
+        "use strict";
+
+        return Controller.extend("com.spm.suppilerportal.controller.VendorSignup", {
+            onInit: function () {
+                // this.getModel("i18n").getProperty('/')
+                var oModel = dataUtil.createJsonModel();
+
+                oModel.setData({
+                    "sNext": true,
+                    "sSubmit": false,
+                    "sLogoImage": sap.ui.require.toUrl("com/spm/suppilerportal/css/image/logo.jpeg")
+                });
+
+                this.getView().setModel(oModel, "oLanding");
+
+            },
+            getResourceBundle: function () {
+                return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            },
+            onClickNext: function () {
+                this.getView().getModel("oLanding").setProperty("/sNext", false);
+                this.getView().getModel("oLanding").setProperty("/sSubmit", true);
+                this.getView().byId("idIconTabBar").setSelectedKey("Contact");
+            },
+            onClickSubmit: function () {
+                this.getView().getModel("oLanding").setProperty("/sNext", true);
+                this.getView().getModel("oLanding").setProperty("/sSubmit", false);
+                this.onCancelForm();
+            },
+            getRouter: function () {
+                return sap.ui.core.UIComponent.getRouterFor(this);
+            },
+            onSignUp: function (oEvent) {
+                // var that = this;
+                // if (!that._oSignUp) {
+                //     that._SignUpDialog = Fragment.load({
+                //         id: that.createId("fSignUpDialog"),
+                //         name: "com.spm.suppilerportal.fragments.VendorSignup",
+                //         controller: that
+                //     }).then(function (oDialog) {
+                //         that._oSignUp = oDialog;
+                //         that.getView().addDependent(that._oSignUp);
+                //         that.getView().getModel("oLanding").setProperty("/sSubmit", false);
+                //     });
+                // }
+                // that._SignUpDialog.then(function (oDialog) {
+                //     that._oSignUp.open();
+
+                // }.bind(that));
+                this.getRouter().navTo("VendorSignup");
+            },
+            onfnpresssubmit: function () {
+                var oModel = this.getView().getModel("oLanding"),
+                    sName = oModel.getProperty("/sUserName"),
+                    sPassword = oModel.getProperty("/sPassword");
+                if (sName !== undefined && sName !== null && sPassword !== undefined && sPassword !== null) {
+                    this.getRouter().navTo("GenericTilesView");
+                    // this.getOwnerComponent().getRouter().navTo("GenericTilesView");
+
+                } else {
+                    sap.m.MessageBox.error("Please enter a valid user name/password");
+                }
+            },
+            onCancelForm: function () {
+                this._oSignUp.close();
+            }
+        });
+    });
