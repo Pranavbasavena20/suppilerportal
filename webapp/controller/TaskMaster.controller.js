f6f88419b443c6b61a9f06690aa0c706dd4a05f5
Pranav Basavena <pranavbasavena20@gmail.com>
2022-11-11 15:32:30 +0800

    Pranav(V1)-Date:11-11-2022

--- webapp/controller/TaskMaster.controller.js
+++ webapp/controller/TaskMaster.controller.js
@@ -16,17 +16,58 @@ sap.ui.define([
                 var oModel = dataUtil.createJsonModel();
 
                 oModel.setData({
-                    "sNext": true,
-                    "sSubmit": false,
-                    "sLogoImage": sap.ui.require.toUrl("com/spm/suppilerportal/css/image/logo.jpeg")
+                    "MasterList": [
+    {
+      Code:"0001",
+	Name:"Unilever",
+	Department:"Household",
+	PrimaryContact:"John",
+	Email:"John@gmail.com",
+	Phone:"123456"
+    },
+     {
+      Code:"0002",
+	Name:"NTUC",
+	Department:"Retail",
+	PrimaryContact:"Abhishek",
+	Email:"Abhishek@gmail.com",
+	Phone:"999999"
+    },
+	{
+      Code:"0003",
+	Name:"NUS",
+	Department:"Retail",
+	PrimaryContact:"Pankaj",
+	Email:"Pankaj@gmail.com",
+	Phone:"9898999"
+    },
+    {
+      Code:"0004",
+	Name:"Gaint",
+	Department:"Retail",
+	PrimaryContact:"Om",
+	Email:"Om@gmail.com",
+	Phone:"996790"
+    }
+        ]
                 });
 
-                this.getView().setModel(oModel, "oLanding");
+                this.getView().setModel(oModel, "oMasterList");
 
             },
             onPress: function (oEvent) {
                 this.getOwnerComponent().getModel("oFiexibleLayout").setProperty("/layout", "TwoColumnsMidExpanded");
                 this.getOwnerComponent().getRouter().navTo("TaskDetail", { TaskType: "1" });
-            }
+            },
+            onPressSortConfirm: function (oEvent) {
+                var sPath, bDescending, aSorters = [],
+                    oTable = this.getView().byId("idMastertable"),
+                    oSortParams = oEvent.getParameters(),
+                    oBinding = oTable.getBinding("items");
+                sPath = oSortParams.sortItem.getKey();
+                bDescending = oSortParams.sortDescending;
+                aSorters.push(new Sorter(sPath, bDescending));
+                oBinding.sort(aSorters);
+            },
         });
     });
